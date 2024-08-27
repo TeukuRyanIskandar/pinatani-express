4ea9e99258882a73b04398be8c4d029bc72367f5
teukurian <teukurian090598@gmail.com>
2024-08-27 14:08:56 +0800

    Add getUserByEmailOrUsername. createUser also creates a profile

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -2,11 +2,12 @@ import {
   CreateUserDto,
   CreateUserResData,
 } from "@lib/types";
-import { httpService } from "@lib/utils";
 import { createUserSchema } from "@models/schema";
 import { userRepository } from "@repositories/user.repository";
+import * as bcrypt from "bcrypt";
 import type { Request, Response } from "express";
 import { z } from "zod";
+import { userProfileRepository } from "../repositories";
 import { BaseService, HttpService } from "./_services";
 
 export class UserService extends BaseService {
@@ -24,23 +25,44 @@ export class UserService extends BaseService {
         createUserSchema.parse(req.body);
 
       const { email, username } = createUserDto;
+      const salt = await bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(
+        createUserDto.password,
+        salt,
+      );
+
+      const newUser = userRepository.create({
+        username: createUserDto.username,
+        email: createUserDto.email,
+        password: hashedPassword,
+      });
+      const savedUser = await userRepository.save(newUser);
+
+      const baseProfile = userRepository.create({
+        username: createUserDto.username,
+        id: savedUser.id,
+      });
+
+      const savedProfile =
+        await userProfileRepository.save(baseProfile);
+
+      savedUser.profileId = savedProfile.profileId;
+
+      await userRepository.save(savedUser);
 
       const existingUser = await userRepository.findOne({
         where: [{ email }, { username }],
       });
 
       if (existingUser) {
-        return httpService(res, {
+        return sendResponse(res, {
           code: 400,
           data: null,
           message: "Email or username already taken",
         });
       }
 
-      const newUser =
-        await userRepository.save(createUserDto);
-
-      return httpService(res, {
+      return sendResponse(res, {
         code: 201,
         data: {
           username: newUser.username,
@@ -67,11 +89,30 @@ export class UserService extends BaseService {
 
   async getUserById(id: number) {
     try {
-      return await userRepository.findOne({
+      const user = await userRepository.findOne({
         where: { id },
       });
+      return user;
     } catch (error) {
       this.logError(error as Error, "getUserById");
+      throw error;
+    }
+  }
+
+  async getUserByEmailOrUsername(emailOrUsername: string) {
+    try {
+      const user = await userRepository.findOne({
+        where: [
+          { email: emailOrUsername },
+          { username: emailOrUsername },
+        ],
+      });
+      return user;
+    } catch (error) {
+      this.logError(
+        error as Error,
+        "getUserByEmailOrUsername",
+      );
       throw error;
     }
   }
